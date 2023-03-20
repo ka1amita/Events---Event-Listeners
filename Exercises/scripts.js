8f95e46bad24dd59787cd7f711fb90855f315293
ka1amita <kalamatej@gmail.com>
2023-03-20 21:40:31 +0100

    complete exercie 2

--- Exercises/scripts.js
+++ Exercises/scripts.js
@@ -6,9 +6,18 @@ $('.second').click(() => {
 $('.first').text('Yeah, you clicked me')
 })
 
+let numberOfClicks = 0
 $('.third').click(() => {
     console.log('click')
+    numberOfClicks ++
+    console.log(numberOfClicks)
+
     let color = $('input').val()
     console.log(color)
-    $('button').css('background-color',color)
+    if (numberOfClicks === 1) {
+    
+        $('button').css('background-color',color)
+    
+}
+
 })
